b6280fcd3b3eaeded179482bd6fd3b5ff4ccad3e
zlisa <837134734@qq.com>
2017-05-19 08:57:21 +0800

    Demo示例更新

--- app/src/main/java/com/zlisa/bottomlayout/MainActivity.java
+++ app/src/main/java/com/zlisa/bottomlayout/MainActivity.java
@@ -1,57 +1,69 @@
 package com.zlisa.bottomlayout;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.zlisa.adapter.ViewPagerAdapter;
 import com.zlisa.widget.BottomLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
-    private ViewPager mViewPager;
     private BottomLayout mBottomLayout;
-    private ViewPagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mViewPager = (ViewPager) findViewById(R.id.view_pager);
         mBottomLayout = (BottomLayout) findViewById(R.id.bottom_layout);
 
         mBottomLayout.addTab(new BottomLayout.Tab("Home",
                 R.mipmap.ic_launcher,
-                R.mipmap.ic_launcher_round));
+                R.mipmap.ic_launcher,
+                R.color.colorPrimary,
+                R.color.colorPrimaryDark));
         mBottomLayout.addTab(new BottomLayout.Tab("Fri",
                 R.mipmap.ic_launcher,
-                R.mipmap.ic_launcher_round));
+                R.mipmap.ic_launcher,
+                R.color.colorPrimary,
+                R.color.colorPrimaryDark));
         mBottomLayout.addTab(null);
         mBottomLayout.addTab(new BottomLayout.Tab("Msg",
                 R.mipmap.ic_launcher,
-                R.mipmap.ic_launcher_round));
+                R.mipmap.ic_launcher,
+                R.color.colorPrimary,
+                R.color.colorPrimaryDark));
         mBottomLayout.addTab(new BottomLayout.Tab("Mine",
                 R.mipmap.ic_launcher,
-                R.mipmap.ic_launcher_round));
+                R.mipmap.ic_launcher,
+                R.color.colorPrimary,
+                R.color.colorPrimaryDark));
 
-        mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        mViewPager.setAdapter(mPagerAdapter);
-        mBottomLayout.setUpWithViewPager(mViewPager);
-
-//        mPagerAdapter.addFragment(TestFragment.newInstance());
-//        mPagerAdapter.addFragment(TestFragment.newInstance());
-//        mPagerAdapter.addFragment(TestFragment.newInstance());
-//        mPagerAdapter.addFragment(TestFragment.newInstance());
+        final TestFragment fragment1 = TestFragment.newInstance("Fragment1");
+        final TestFragment fragment2 = TestFragment.newInstance("Fragment2");
+        final TestFragment fragment3 = TestFragment.newInstance("Fragment3");
+        final TestFragment fragment4 = TestFragment.newInstance("Fragment4");
 
         mBottomLayout.setOnTabSelectListener(new BottomLayout.OnTabSelectListener() {
             @Override
             public void onTabSelected(BottomLayout.Tab tab) {
                 Log.i("MainActivity", "tab : " + tab.getPosition());
+                switch (tab.getPosition()) {
+                    case 0:
+                        replaceFragment(R.id.container, fragment1);
+                        break;
+                    case 1:
+                        replaceFragment(R.id.container, fragment2);
+                        break;
+                    case 2:
+                        replaceFragment(R.id.container, fragment3);
+                        break;
+                    case 3:
+                        replaceFragment(R.id.container, fragment4);
+                        break;
+                }
             }
         });
 
-        mBottomLayout.setCurrentPosition(0);
+        mBottomLayout.setUp();
     }
 }
